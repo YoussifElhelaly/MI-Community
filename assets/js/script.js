1692fb8def4dac3d4169fbfad72b2b8ddbca445b
YoussifElhelaly <89140149+YoussifElhelaly@users.noreply.github.com>
2023-05-07 23:59:25 +0300

    finish profile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,47 +2,58 @@ let PostOptionBTN = document.getElementById("PostOptionBTN")
 let postOptionMenu = document.getElementById("postOptionMenu")
 let lovePost = document.getElementById("lovePost")
 let savePost = document.getElementById("savePost")
-let notifi = document.getElementById("notifi")
 let lovePostParent = document.getElementById("lovePostParent")
 let commentPostParent = document.getElementById("commentPostParent")
 let commentSection = document.getElementById("commentSection")
 let savePostParent = document.getElementById("savePostParent")
-let notifictionWrapper = document.getElementById("notifictionWrapper")
 let special = document.getElementById("special")
 let specialNav = document.getElementById("specialNav")
+let aboutOptionBTN = document.getElementById("aboutOptionBTN")
+let aboutOptionMenu = document.getElementById("aboutOptionMenu")
+let introOptionBTN = document.getElementById("introOptionBTN")
+let introOptionMenu = document.getElementById("introOptionMenu")
+let insertOptionBTN = document.getElementById("insertOptionBTN")
+let insertOptionMenu = document.getElementById("insertOptionMenu")
 
-PostOptionBTN.addEventListener("click", () => {
+PostOptionBTN?.addEventListener("click", () => {
     postOptionMenu.classList.toggle("active")
 })
 
-lovePostParent.addEventListener("click", () => {
+lovePostParent?.addEventListener("click", () => {
     lovePostParent.classList.toggle("active")
     lovePost.classList.toggle("fa-solid")
 })
 
-savePostParent.addEventListener("click", () => {
+savePostParent?.addEventListener("click", () => {
     savePostParent.classList.toggle("active")
     savePost.classList.toggle("fa-solid")
 })
 
-commentPostParent.addEventListener("click", () => {
+commentPostParent?.addEventListener("click", () => {
     commentPostParent.classList.toggle("active")
     commentSection.classList.toggle("active")
 })
 
 
-notifi.addEventListener("click", () => {
+notifi?.addEventListener("click", () => {
     notifi.classList.toggle("active")
 })
 
-addEventListener("click", (e) => {
-    console.log(e.target)
-})
-
-special.addEventListener("mouseenter", () => {
+special?.addEventListener("mouseenter", () => {
     specialNav.style.width = 'calc(100%/12 *2)'
 })
 
-special.addEventListener("mouseleave", () => {
+special?.addEventListener("mouseleave", () => {
     specialNav.style.width = '120px'
+})
+
+aboutOptionBTN?.addEventListener("click", () => {
+    aboutOptionMenu.classList.toggle("active")
+})
+
+introOptionBTN?.addEventListener("click", () => {
+    introOptionMenu.classList.toggle("active")
+})
+insertOptionBTN?.addEventListener("click", () => {
+    insertOptionMenu.classList.toggle("active")
 })
